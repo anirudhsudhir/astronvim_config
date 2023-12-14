e7792e6d5c44d45ccac24fa845596b923133ace9
Anirudh Sudhir <137381184+anirudhsudhir@users.noreply.github.com>
2023-12-14 19:29:04 +0530

    refactor commmunity.lua

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,17 +4,35 @@ return {
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-   { import = "astrocommunity.color.transparent-nvim" },
-   
-   { import = "astrocommunity.colorscheme.catppuccin" },
-   { import = "astrocommunity.colorscheme.tokyonight-nvim" },
+  { import = "astrocommunity.color.transparent-nvim" },
 
-   { import = "astrocommunity.markdown-and-latex.markdown-preview-nvim" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  {
+    "catppuccin",
+    opts = {
+      integrations = {
+        sandwich = false,
+        noice = true,
+        mini = true,
+        leap = true,
+        markdown = true,
+        neotest = true,
+        cmp = true,
+        overseer = true,
+        lsp_trouble = true,
+        rainbow_delimiters = true,
+      },
+    },
+  },
+  { import = "astrocommunity.colorscheme.tokyonight-nvim" },
 
-   { import = "astrocommunity.pack.cpp" },
-   { import = "astrocommunity.pack.go" },
-   { import = "astrocommunity.pack.html-css" },
-   { import = "astrocommunity.pack.markdown" },
-   { import = "astrocommunity.pack.python" },
-   { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.markdown-and-latex.markdown-preview-nvim" },
+
+  { import = "astrocommunity.pack.cpp" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.lua" },
 }
